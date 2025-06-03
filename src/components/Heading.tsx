8d64c270d15eeb12ee240ecf4149b4d897e4ef53
Gavin Murambadoro <gmurambadoro1@gmail.com>
2025-06-03 22:17:11 +0200

    feat: show character details

--- src/components/Heading.tsx
+++ src/components/Heading.tsx
@@ -2,7 +2,7 @@ import { AppBar, Toolbar, Typography } from "@mui/material";
 
 export default function Heading() {
   return (
-    <AppBar position="static" sx={{ mb: 2, mt: 2 }}>
+    <AppBar position={"static"} sx={{ mb: 2, mt: 2 }}>
       <Toolbar>
         <Typography variant={"h5"} color={"inherit"} component={"div"}>
           The Office - A Mockumentary Series
